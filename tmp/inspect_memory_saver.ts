e1dfe9375856863a83db913e3d01714b0e60fb6a
sanderok-gen <joryk885@gmail.com>
2026-04-03 19:00:15 +0800

    chore: remove temp, scratch and AI workflow files from tracking

--- tmp/inspect_memory_saver.ts
+++
@@ -1,4 +0,0 @@
-import { MemorySaver } from "@langchain/langgraph";
-const saver = new MemorySaver();
-console.log("MemorySaver methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(saver)));
-console.log("MemorySaver properties:", Object.getOwnPropertyNames(saver));
